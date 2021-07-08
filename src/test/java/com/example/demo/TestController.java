67103833a7ba8fd6e76b019c533eb9d0e69e0224
devanle <7devanle@gmail.com>
2021-07-08 02:15:03 +0100

    changes

--- src/test/java/com/example/demo/TestController.java
+++ src/test/java/com/example/demo/TestController.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class TestController {
 	@Mock 
 	private UuidRepo repo;
