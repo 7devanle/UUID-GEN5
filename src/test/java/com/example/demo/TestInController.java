3b84c70af483bef770682856e0f3cef38fb07198
devanle <7devanle@gmail.com>
2021-07-08 04:56:46 +0100

    changes

---
+++ src/test/java/com/example/demo/TestInController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestInController {
+
+	@Test
+	void testList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListOfUuids() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListOfUuidsUUID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreatUuid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteuuid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateuuid() {
+		fail("Not yet implemented");
+	}
+
+}
